948ca076167c8bed59eb2ea7ad06bf1fe2acd6c0
Pierre <pedevineau@gmail.com>
2017-11-10 16:00:46 +0100

    catch exception if slice is too short by filling it with empty

--- choose_training_sample.py
+++ choose_training_sample.py
@@ -1,8 +1,8 @@
-from numpy import shape, empty, linspace, sum
+from numpy import shape, empty, linspace, sum, mean, var
 from numpy.random import randint, shuffle
 
 
-def get_random_stratified_samples(array, training_rate, number_of_layers):
+def get_random_stratified_samples(array, training_rate, number_of_layers, me, std):
     nb_slots, nb_lats, nb_lons, nb_features = shape(array)
     subdivisions = linspace(0, nb_slots, number_of_layers+1, endpoint=True, dtype=int)  # supposed to be integers
     pis = empty(number_of_layers)
@@ -14,7 +14,7 @@ def get_random_stratified_samples(array, training_rate, number_of_layers):
             lat = randint(0, nb_lats)
             lon = randint(0, nb_lons)
             slot = randint(subdivisions[k], subdivisions[k+1])
-            if not looks_like_night(array[slot, lat, lon]):
+            if not looks_like_night(array[slot, lat, lon], me, std):
                 ck += 1
         pis[k]=ck
     s = 1.*sum(pis)
@@ -30,20 +30,25 @@ def get_random_stratified_samples(array, training_rate, number_of_layers):
         array_to_return[nb_points_picked:nb_points_picked + nb_points_to_pick] = slice_for_picking[0:nb_points_to_pick]
         nb_points_picked += nb_points_to_pick
         if subdivisions[k + 1] == nb_slots and nb_points_picked < total_training_len:  # possible because of rounded errors
-            array_to_return[nb_points_picked:total_training_len] \
-                = slice_for_picking[nb_points_to_pick:nb_points_to_pick+total_training_len-nb_points_picked]
+            try:
+                array_to_return[nb_points_picked:total_training_len] \
+                    = slice_for_picking[nb_points_to_pick:nb_points_to_pick+total_training_len-nb_points_picked]
+            except:
+                continue
     return array_to_return
 
 
-def looks_like_night(data):
+def looks_like_night(point, me, std):
     ## wait for array such as [0,17,-25,3]
-    for d in data:
-        if d != 0:
+    for k in range(len(point)-1):
+        m = me[k]
+        s = std[k]
+        if abs(point[k]*s + m) > 0.001:
             return False
     return True
 
 
-def evaluate_randomization(array):
+def evaluate_randomization(array, me, std):
     nb_slots, nb_lats, nb_lons, nb_features = shape(array)
     count = 0
     N = 500
@@ -51,7 +56,7 @@ def evaluate_randomization(array):
         lat = randint(0, nb_lats)
         lon = randint(0, nb_lons)
         slot = randint(0, nb_slots)
-        if not looks_like_night(array[slot, lat, lon]):
+        if not looks_like_night(array[slot, lat, lon], me, std):
             count += 1
     print 'COUNT IS'
     print count
