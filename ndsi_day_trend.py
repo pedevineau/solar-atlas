e1b5ce35a69e262a548507dbe6f06a86ddcc480f
Pierre <pedevineau@gmail.com>
2017-11-16 09:52:01 +0100

    slight optimization. compute percentile only if necessary

--- ndsi_day_trend.py
+++ ndsi_day_trend.py
@@ -34,20 +34,22 @@ from utils import *
 #     plt.show()
 
 
-def recognize_pattern(ndsi, mu, mask, time_step_satellite, slices_by_day=1, tolerance=0.2):
+def recognize_pattern(ndsi, mu, mask, time_step_satellite, slices_by_day=1, tolerance=0.1):
     print 'begin recognize pattern'
     from time import time
     t_begin_reco = time()
-    from scipy.stats import pearsonr
-    from numpy import zeros, shape, percentile
-
     # https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.pearsonr.html
+    from scipy.stats import pearsonr
+    from numpy import percentile, shape
 
-    (a,b,c) = shape(ndsi)
+    # computing of correlation need enough temporal information. If we have data on a too small window, ignore it
+    minimal_nb_unmasked_slots = 12
 
-    # we'll return a discrete information giving supposed class, and a countinuous
-    # classes: 1 for anow, -1 for clouds, 0 otherwise
-    to_return = zeros((a,b,c,2))
+
+    # we'll return a "doped" ndsi. cloudy looks like -1, snowy looks like 1, and other situations are not changed
+    # classes: 1 for snow, -1 for clouds, 0 otherwise
+    (a, b, c) = shape(ndsi)
+    to_return = ndsi
     nb_slots_by_day = get_nb_slots_by_day(time_step_satellite)
     nb_days = a/nb_slots_by_day
     nb_steps = slices_by_day * nb_days
@@ -58,27 +60,29 @@ def recognize_pattern(ndsi, mu, mask, time_step_satellite, slices_by_day=1, tole
                 slice_ndsi = ndsi[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude]
                 slice_mu = mu[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude]
                 slice_mask = mask[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude]
-                if slice_ndsi[~slice_mask].size != 0:
-                    low_summit_clouds = percentile(slice_ndsi[~slice_mask], 5)
+                if slice_ndsi[~slice_mask].size > minimal_nb_unmasked_slots:
                     p_snow, r_snow = pearsonr(
                         slice_ndsi[~slice_mask],
                         slice_mu[~slice_mask]
                     )
-                    if -p_snow > 1 - tolerance:   # priority is given to clouds rather than to snow
-                        to_return[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude, 0] = -1  # -1 is cloud
-                        to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude, 1] = \
+                    if -p_snow > 1 - tolerance:  # if it is anti-correlated with cos zen
+                        # percentile computing enables to find the summit of the convex function
+                        low_summit_clouds = percentile(slice_ndsi[~slice_mask], 5)
+                        # to_return[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude, 1] = -1  # -1 is cloud
+                        to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude] = \
                             - (2+slice_ndsi) / (3+low_summit_clouds - slice_mu)
                         # the previous +2 and +3 (=+2+1) serves numerical stability
-                    elif p_snow > 1 - tolerance:
+                    elif p_snow > 1 - 2*tolerance:
                         # print 'snow'
                         # print r_snow
-                        to_return[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude, 0] = 1  # 1 is snow
-                        to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude, 1] = \
+                        # to_return[k*nb_slots_by_step:(k+1)*nb_slots_by_step, latitude, longitude, 1] = 1  # 1 is snow
+                        to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude] = \
                             slice_ndsi / slice_mu
-                    else:
-                        to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude, 1] = slice_ndsi
+                    # already computed before the loop
+                    # else:
+                    #     to_return[k * nb_slots_by_step:(k + 1) * nb_slots_by_step, latitude, longitude] = slice_ndsi
     print 'time recognition', time() - t_begin_reco
-    return to_return
+    return to_return[:,:,:]
 
 
 
