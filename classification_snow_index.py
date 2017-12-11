add dark cli, remove 1-d variability (generated a lag of 1 day...), fix serious normalization issue in remove_cos_zen_component

classification_snow_index.py
@@ -29,8 +29,8 @@ def classify_brightness(bright_index, m, s):
     brightness = model.predict(bright_index_1d).reshape((nb_slots, nb_latitudes, nb_longitudes))
     centers3 = get_centers(model, process)
     [undefined, dark, bright] = np.argsort(centers3.flatten())
-    if centers3[dark, 0] + get_std(model, process, dark) >\
-            centers3[bright, 0]:
+    if centers3[bright, 0] - centers3[dark, 0] < \
+            1.2*max(get_std(model, process, dark), get_std(model, process, bright)):
         print 'bad separation between bright and dark'
         print 'using awful thresholds instead'
         brightness = np.zeros((nb_slots, nb_latitudes, nb_longitudes))
@@ -61,7 +61,7 @@ def classifiy_brightness_variability(bright_variability):
     bright_index_training = brightness_copy[:nb_samples]
     del brightness_copy
     from naive_gaussian_classification import get_basis_model, get_trained_model
-    process = 'kmeans'
+    process = 'bayesian'
     print 'classify brightness_variability variability', process
     nb_components = 3
     max_iter = 300

get_data.py
@@ -123,18 +123,21 @@ def apply_smooth_threshold(x, th, order=2):
     return np.exp(-(x-th))
 
 
-def compute_short_variability(array, cos_zen, mask, step=1, return_mask=False, abs_value=False, normalization='standard'):
+def compute_short_variability(array, cos_zen, mask, step=1, return_mask=False, abs_value=False,
+                              option=None,  # [None, 'without-bias']
+                              normalization='standard'):
     step_left = step
-    array = remove_cos_zen_correlation(array, normalize_array(cos_zen, mask, normalization='standard'), mask)
+    if option == 'without-bias':
+        array = remove_cos_zen_correlation(array, cos_zen, mask)
     previous = np.roll(array, step_left, axis=0)
     array = array - previous
     if mask is not None:
         mask = mask + np.roll(mask, step_left, axis=0)  # mask of night and dawn. numpy.roll casts the mask to an array
-    # array[:step_left] = 0
+        mask[:step_left] = True
     if abs_value:
         array = np.abs(array)
     if mask is not None:
-        array[mask] = 0
+        array[mask] = -10
     array = normalize_array(array, mask=mask, normalization=normalization)
     if return_mask:
         return array, mask
@@ -142,20 +145,25 @@ def compute_short_variability(array, cos_zen, mask, step=1, return_mask=False, a
         return array
 
 
-def remove_cos_zen_correlation(indexstd, mustd, mask):
-    to_return = np.zeros_like(indexstd)
+# def compute_intra_variability(*groups):
+#     intra = []
+#     inter = 0
+#     for group in groups:
+
+
+
+def remove_cos_zen_correlation(index, cos_zen, mask):
+    to_return = index.copy()
     (nb_slots, nb_latitudes, nb_longitudes) = np.shape(to_return)
+    from scipy.stats import linregress
     for lat in range(nb_latitudes):
         for lon in range(nb_longitudes):
-            slice_diffstd = indexstd[:, lat, lon]
-            slice_mustd = mustd[:, lat, lon]
             slice_maski = mask[:, lat, lon]
+            slice_diffstd = index[:, lat, lon][~slice_maski]
+            slice_mustd = cos_zen[:, lat, lon][~slice_maski]
             if not np.all(slice_maski):
-                local_cov_matrix = np.cov(slice_diffstd[~slice_maski], slice_mustd[~slice_maski])
-                local_cov = local_cov_matrix[0, 1]
-                local_var_mu = local_cov_matrix[1, 1]
-                local_var_cli = local_cov_matrix[0, 0]
-                to_return[:, lat, lon] = slice_diffstd - local_cov / np.sqrt(local_var_mu * local_var_cli) * slice_mustd
+                slope = linregress(slice_mustd, slice_diffstd)[0]
+                to_return[:, lat, lon][~slice_maski] -= slope * slice_mustd
     return to_return
 
 

infrared_predictors.py
@@ -34,15 +34,16 @@ def get_infrared_predictors(array_data, times, latitudes, longitudes, satellite_
     mu[mu < 0] = 0
 
     array_indexes = np.empty(shape=(nb_slots, nb_latitudes, nb_longitudes, nb_features))
-    array_indexes[:, :, :, 0] = median_filter_3d(
+    # remove spatial smoothing
+    array_indexes[:, :, :, 0] = \
         get_cloud_index(mir=array_data[:, :, :, 1], fir=array_data[:, :, :, 0], method='mu-normalization',
-                        mask=mask, cos_zen=mu),
-        scope=3)
+                        mask=mask, cos_zen=mu)
 
-    array_indexes[:, :, :, 1] = median_filter_3d(
+
+    array_indexes[:, :, :, 1] = \
         get_cloud_index(mir=array_data[:, :, :, 1], fir=array_data[:, :, :, 0], method='without-bias',
-                        mask=mask, cos_zen=mu),
-        scope=3)
+                        mask=mask, cos_zen=mu)
+
 
     # array_indexes[:, :, :, 3] = get_variability_array(array=array_indexes[:, :, :, 2], mask=mask_cli)
     #
@@ -95,19 +96,16 @@ def get_cloud_index(mir, fir, mask, cos_zen, method='default'):
         cli = normalize_array(difference / np.maximum(cos_zen, mu_threshold),
                               mask=mask, normalization='standard', return_m_s=False)
     else:
-        diffstd = normalize_array(difference, mask, normalization='standard', return_m_s=False)
-        mustd = normalize_array(cos_zen, mask, normalization='standard', return_m_s=False)
+        diffstd = normalize_array(difference, mask, normalization='standard')
         if method == 'without-bias':
             from get_data import remove_cos_zen_correlation
-            cli = remove_cos_zen_correlation(diffstd,  mustd, mask)
+            cli = remove_cos_zen_correlation(diffstd,  cos_zen, mask)
         elif method == 'clear-sky':
-            cli = diffstd-mustd
+                cli = diffstd-normalize_array(cos_zen, mask, normalization='standard')
         elif method == 'default':
             cli = diffstd
         else:
             raise Exception('Please choose a valid method to compute cloud index')
-        print 'remaining pearson', pearsonr(cli[~mask],
-                                            mustd[~mask])  # objective: put it as close to zero as possible
     cli[mask] = -10
     # cli, m, s = normalize_array(cli, maski, normalization='max')
     return cli

visible_predictors.py
@@ -24,15 +24,20 @@ def get_visible_predictors(array_data, ocean_mask, times, latitudes, longitudes,
                                            mask=mask, mu=mu, threshold_mu=0.02, ocean_mask=ocean_mask,
                                            return_m_s_mask=True)
 
+    # print 'replace NDSI by VIS (en sekrd)'
+    # ndsi, m, s = normalize_array(array=array_data[:, :, :, 1], mask=mask_ndsi, normalization='standard', return_m_s=True)
+
     del array_data
 
-    var_ndsi_1 = compute_short_variability(array=ndsi, cos_zen=mu, mask=mask_ndsi, step=1, return_mask=False, abs_value=True)
-    var_ndsi_144 = compute_short_variability(array=ndsi, cos_zen=mu, mask=mask_ndsi, step=144, return_mask=False, abs_value=True)
+    var_ndsi_1 = compute_short_variability(array=ndsi, cos_zen=mu, mask=mask_ndsi,
+                                           step=1, option='without-bias', return_mask=False, abs_value=True)
+    # var_ndsi_144 = compute_short_variability(array=ndsi, cos_zen=mu, mask=mask_ndsi, step=144, return_mask=False, abs_value=True)
 
 
     # array_indexes[:, :, :, 1] = median_filter_3d(np.maximum(var_ndsi_1, var_ndsi_2), scope=0)
-    array_indexes[:, :, :, 1] = np.maximum(var_ndsi_1, var_ndsi_144)
-    array_indexes[:, :, :, 0] = median_filter_3d(ndsi, scope=0)
+    # array_indexes[:, :, :, 1] = np.maximum(var_ndsi_1, var_ndsi_144)
+    array_indexes[:, :, :, 1] = var_ndsi_1
+    array_indexes[:, :, :, 0] = ndsi
 
     if weights is not None:
         for feat in range(nb_features):