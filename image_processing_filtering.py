88cef58267e6d6ffec56ce3460ed8275f96d79a3
pedevineau <pedevineau@gmail.com>
2018-01-03 13:36:56 +0100

    watershed #2

--- image_processing_filtering.py
+++ image_processing_filtering.py
@@ -24,6 +24,8 @@ def segmentation(features, chan):
 
 
 def segmentation_watershed(image):
+    # Image Segmentation with Watershed Algorithm
+    # http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_watershed/py_watershed.html
     from scipy import ndimage
     from skimage.feature import peak_local_max
     from skimage.morphology import watershed
@@ -32,30 +34,46 @@ def segmentation_watershed(image):
     import cv2
     import matplotlib.pyplot as plt
 
-    # construct the argument parse and parse the arguments
-    # load the image and perform pyramid mean shift filtering
-    # to aid the thresholding step
-    shifted = cv2.pyrMeanShiftFiltering(image, 21, 51)
-    # convert the mean shift image to grayscale, then apply
-    # Otsu's thresholding
-    gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
-    thresh = apply_otsu(gray)[0]
-    D = ndimage.distance_transform_edt(thresh)
-    localMax = peak_local_max(D, indices=False, min_distance=20,
-                              labels=thresh)
+    # noise removal
+    thresh = apply_otsu(image)[0]
 
-    # perform a connected component analysis on the local peaks,
-    # using 8-connectivity, then appy the Watershed algorithm
-    markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-    labels = watershed(-D, markers, mask=thresh)
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+    # Add one to all labels so that sure background is not 0, but 1
+    markers += 1
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+
+    # D = ndimage.distance_transform_edt(thresh)
+    # localMax = peak_local_max(D, indices=False, min_distance=20,
+    #                           labels=thresh)
+
+    labels = cv2.watershed(image, markers)
+    image[markers == -1] = [255, 0, 0]
+
     # loop over the unique labels returned by the Watershed
     # algorithm
     fig, ax = plt.subplots()
-    contours = find_contours(np.unique(labels), 1)
+    contours = find_contours(labels, 1)
     for n, contour in enumerate(contours):
         ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
     ax.imshow(image, cmap=plt.cm.gray)
     plt.show()
+
 
 def apply_otsu(img):
     import cv2
