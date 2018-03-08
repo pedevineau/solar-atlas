b6cd9642775041358f419c9496b264e943850555
Pierre <pedevineau@gmail.com>
2018-03-08 11:28:28 +0100

    fix the bug: the same test was done twice

--- evaluation_static_tests.py
+++ evaluation_static_tests.py
@@ -1,28 +1,18 @@
 def corr2test(test_1, test_2):
     # expect list
-    try:
-        test_1 = test_1.flatten()
-        test_2 = test_2.flatten()
-    except:
-        pass
+    # test_1 = test_1.flatten()
+    # test_2 = test_2.flatten()
     assert len(test_1) == len(test_2), 'the two lists have different sizes'
     score = (1.*sum(test_1 == test_2)) / len(test_1)
-    print score
     return score
 
 
-def unique_scores(*args):
+def common_scores(*args):
     # expect list
     n = len(args)
-    try:
-        common = args[0].flatten()
-    except:
-        common = args[0]
+    common = args[0]
     for k in range(1, n):
-        try:
-            common = (args[k].flatten() | common)
-        except:
-            common = (common | args[k])
+        common = (args[k] | common)
     score = []
     for l in range(n):
         score.append(corr2test(common, args[l]))
@@ -37,20 +27,20 @@ def visualize_regular_cloud_tests():
     :return:
     '''
     from static_tests import dawn_day_test, cli_water_cloud_test, cli_stability, gross_cloud_test, thin_cirrus_test, \
-        typical_static_classifier
+        typical_static_classifier, epsilon_transparent_cloud_test
     from numpy import zeros_like
     from utils import visualize_map_time, typical_bbox
     zen, lands, cli_mu, cli_var, cli_epsilon, vis, lir, fir, lir_forecast, fir_forecast = typical_static_classifier(
         bypass=True)
     common_condition = lands & dawn_day_test(zen)
     cli_condition = (cli_water_cloud_test(cli_mu) & cli_stability(cli_var))
-    epsilon_condition = (cli_water_cloud_test(cli_mu) & cli_stability(cli_var))
+    epsilon_condition = epsilon_transparent_cloud_test(cli_epsilon)
     gross_condition = gross_cloud_test(lir, lir_forecast)
     thin_condition = thin_cirrus_test(lands, lir, fir, lir_forecast, fir_forecast)
-    scores = unique_scores(
+    scores = common_scores(
+        gross_condition[common_condition],
         cli_condition[common_condition],
         epsilon_condition[common_condition],
-        gross_condition[common_condition],
         thin_condition[common_condition],
     )
     print 'SCORES FOR LAND DAWN-DAY PRIMARY CLOUD CLASSIFICATION'
